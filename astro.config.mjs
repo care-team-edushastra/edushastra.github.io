6f1a22094cfd14e5f21e3b8486ded8b76cf36035
care-team-edushastra <care@edushastra.com>
2026-08-18 13:24:40 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   // Your main URL
   site: 'https://edushastra.github.io',
-
   // Leave this as an empty string for a User Site (edushastra.github.io)
   base: '', 
-  
+
+  build: {
+    inlineStylesheets: 'always',
+  },
+
   vite: {
     plugins: [tailwindcss()],
   },
